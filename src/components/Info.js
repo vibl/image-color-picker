f161d6f6333b4bf5b318108f54db2e7df95dc6aa
Vianney Stroebel <628818+vibl@users.noreply.github.com>
2020-11-11 08:23:09 +0300

    Logo updated

--- src/components/Info.js
+++ src/components/Info.js
@@ -189,20 +189,6 @@ function Info({ selectedColors, setSelectedColors }) {
             <IconDribble />
           </a>
         </div>
-        <div className="product-hunt" style={{ marginTop: "12px" }}>
-          <a
-            href="https://www.producthunt.com/posts/image-color-picker?utm_source=badge-featured&utm_medium=badge&utm_souce=badge-image-color-picker"
-            target="_blank"
-          >
-            <img
-              src="https://api.producthunt.com/widgets/embed-image/v1/featured.svg?post_id=274059&theme=light"
-              alt="Image Image Image Color Pickererer - Pick colors from an image | Product Hunt"
-              style={{ width: "250px", height: "54px" }}
-              width="250"
-              height="54"
-            />
-          </a>
-        </div>
       </footer>
     </div>
   );
